670d23dc4e9839240243a0961b81b79f71d0e988
Rithy <huotrithy01@gmail.com>
2024-02-18 21:05:55 +0700

    Create calculator

--- Script.js
+++ Script.js
@@ -1,24 +1,48 @@
+const convertToNum = function(value) {
+    try {
+        value = value * 1;
+    }
+    catch {
+        console.log(`${value} is not a number`)
+        value = 0;
+    }
+    return value;
+}
+
 const add = function(num1, num2) {
+    num1 = convertToNum(num1);
+    num2 = convertToNum(num2);
     return num1 + num2;
 }
 
 const subtract = function(num1, num2) {
+    num1 = convertToNum(num1);
+    num2 = convertToNum(num2);
     return num1 - num2;
 }
 
 const multiply = function(num1, num2) {
+    num1 = convertToNum(num1);
+    num2 = convertToNum(num2);
     return num1 * num2;
 }
 
 const divide = function(num1, num2) {
+    num1 = convertToNum(num1);
+    num2 = convertToNum(num2);
     return num1 / num2;
+}
+
+const sqareRoot = function(num) {
+    num = convertToNum(num)
+    return Math.sqrt(num)
 }
 
 let num1;
 let num2;
 let operator;
 
-const operate = function(num1, num2, operate) {
+const operate = function(num1, num2 = 0, operate) {
     let result;
     switch(operate) {
         case '+':
@@ -34,25 +58,134 @@ const operate = function(num1, num2, operate) {
         case '/':
             result = divide(num1, num2);
             break;
+        case '√':
+            result = sqareRoot(num1);
+            break;
+        default:
+            console.log("No operator");
+            result = 0;
+            break;
     }
     return result;
 }
 
-let inputNum1;
+// Display value into calculator box
+const displayValue = function(value) {
+    
+    const calculatorBox = document.querySelector("input"); 
+    
+    // Format and display to inpubox
+    calculatorBox.value = calculatorBox.value += value;
 
-const calculatorBox = document.querySelector("input"); 
+    // Scroll to left ot display latest input number
+    calculatorBox.scrollLeft = calculatorBox.scrollWidth;
+}
+
+// Clear calculator box
+const clearDisplay = function() {
+    const calculatorBox = document.querySelector("input"); 
+    calculatorBox.value = "";
+}
+
+const clearAll = function() {
+    const calculatorBox = document.querySelector("input"); 
+    calculatorBox.value = "";
+    
+    inputNum1 = 0;
+    inputNum2 = 0;
+}
+
+const retrieveValue = function() {
+    const calculatorBox = document.querySelector("input"); 
+    return calculatorBox.value;
+}
+
+const backward = function() {
+    value = retrieveValue();
+    value = value.slice(0, value.length - 1);
+    clearDisplay()
+    displayValue(value);
+    return value;
+}
+
+const equal = function(value) {
+    displayValue(value);
+}
+
+let inputNum1;
+let inputNum2;
+let inputOperator;
+let result;
+let specialInputOperator;
+
 const buttonContainer = document.querySelector(".container");
 buttonContainer.addEventListener("click", (e) => {
     
     // User click on number value
     if (e.target.classList.contains("value")) {
-        // Display to inpubox
-        calculatorBox.value += e.target.textContent;
+        displayValue(e.target.textContent);
+    }
+    // User click on operator
+    else if (e.target.classList.contains("operator")) {
 
-        // Scroll to left ot display latest input number
-        calculatorBox.scrollLeft = calculatorBox.scrollWidth;
+        if (result) {
+            clearDisplay();
+        }
 
         // Store input value in a variable
-        inputNum1 = calculatorBox.value;
+        if (!inputNum1) {
+            inputNum1 = retrieveValue();
+            clearDisplay();
+        }
+        else {
+            inputNum2 = retrieveValue();            
+            clearDisplay();
+        }
+        
+        // Calculate result if user click on operator again and two input value is filled
+        if (inputNum1 && inputNum2 && inputOperator) {
+            result = operate(inputNum1, inputNum2, inputOperator);
+            clearAll();
+            displayValue(result);
+            inputNum1 = result;
+        }
+
+        inputOperator = e.target.textContent;
+        
+        if (inputOperator === '√') {
+            result = operate(inputNum1, undefined, inputOperator);
+            clearAll();
+            displayValue(result);
+            inputNum1 = result;
+        }
+    }
+    else {
+        specialInputOperator = e.target.textContent;
+        switch(specialInputOperator) {
+            case 'C':
+                clearAll();
+                break;
+            case '⌫':
+                if (inputNum2) {
+                    inputNum2 = backward();
+                }
+                else {
+                    inputNum1 = backward()
+                }
+                break;
+            case '=':
+                if (!inputNum2) {
+                    inputNum2 = retrieveValue();
+                }
+
+                if (inputNum1 && inputNum2) {
+                    result = operate(inputNum1, inputNum2, inputOperator);
+                    clearAll();
+                    displayValue(result);
+                    inputNum1 = result;
+                }
+                break;
+                
+        }
     }
 });
