025fdb3e2094bdba6673a6113f579529b2ecc661
Rithy <huotrithy01@gmail.com>
2024-02-18 10:30:21 +0700

    Create operate function to call the calculate function

--- Script.js
+++ Script.js
@@ -1,15 +1,39 @@
-const add = function(a, b) {
-    return a + b;
+const add = function(num1, num2) {
+    return num1 + num2;
 }
 
-const subtract = function(a, b) {
-    return a - b;
+const subtract = function(num1, num2) {
+    return num1 - num2;
 }
 
-const multiply = function(a, b) {
-    return a * b;
+const multiply = function(num1, num2) {
+    return num1 * num2;
 }
 
-const divide = function(a, b) {
-    return a / b;
+const divide = function(num1, num2) {
+    return num1 / num2;
+}
+
+let num1;
+let num2;
+let operator;
+
+const operate = function(num1, num2, operate) {
+    let result;
+    switch(operate) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
 }
